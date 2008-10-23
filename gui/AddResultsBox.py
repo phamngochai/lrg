e0b75f1e6e4a12132375f4b079a2b0717dca532a
Pham Ngoc Hai <ngochai@gmail.com>
2008-10-23 17:10:17 +0000

    fixed abort all bug, added addURL results box

---
+++ gui/AddResultsBox.py
@@ -0,0 +1,27 @@
+import wx
+ID_BUTOK = 101
+
+class AddResultsBox(wx.Frame):
+	def __init__(self, parent, id, title, text):
+		wx.Frame.__init__(self, parent, id, title)
+		
+		self.sizerVertical = wx.BoxSizer(wx.VERTICAL)
+		self.textBox = wx.TextCtrl(self, wx.ID_ANY, style = wx.TE_MULTILINE)
+		self.textBox.SetValue(text)
+		
+		self.buttonOK = wx.Button(self, ID_BUTOK, 'OK')
+		wx.EVT_BUTTON(self, ID_BUTOK, self.OnClickOk)
+		
+		self.sizerHorizontal = wx.BoxSizer(wx.HORIZONTAL)
+		self.sizerHorizontal.Add(self.buttonOK, 1, wx.EXPAND)
+	
+		self.sizerVertical.Add(self.textBox, 1, wx.EXPAND)
+		self.sizerVertical.Add(self.sizerHorizontal, 0, wx.ALIGN_CENTER)
+		self.SetSizer(self.sizerVertical)
+		
+		self.SetAutoLayout(True)
+		self.Center(wx.BOTH)		
+		self.Show(True)
+		
+	def OnClickOk(self, event):
+		self.Destroy()
